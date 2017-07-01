49ff4b1026f8c1aa0e7665d74aac9f5c72bee841
INNOVATIVE-MONS\Innovative Monsters <lee@pkpictures.com>
2017-07-01 17:43:49 -0500

    take out teleport mobile

--- server/static/js/spawn-in-circle.component.js
+++ server/static/js/spawn-in-circle.component.js
@@ -3,20 +3,42 @@ AFRAME.registerComponent('spawn-in-circle', {
     radius: {type: 'number', default: 1}
   },
 
-  init: function() {
-    var el = this.el;
-    var center = el.getAttribute('position');
+/**/
 
-    var angleRad = this.getRandomAngleInRadians();
-    var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
-    var worldPoint = {x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z};
-    el.setAttribute('position', worldPoint);
+init: function () {
 
-    var angleDeg = angleRad * 180 / Math.PI;
-    var angleToCenter = -1 * angleDeg + 90;
-    var rotationStr = '0 ' + angleToCenter + ' 0';
-    el.setAttribute('rotation', rotationStr);
-  },
+        if( AFRAME.utils.device.isMobile() ){
+
+          var el = this.el;
+          var center = el.getAttribute('position');
+
+          var angleRad = this.getRandomAngleInRadians();
+          var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
+          var worldPoint = {x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z};
+          el.setAttribute('position', worldPoint);
+
+          var angleDeg = angleRad * 180 / Math.PI;
+          var angleToCenter = -1 * angleDeg + 90;
+          var rotationStr = '0 ' + angleToCenter + ' 0';
+          el.setAttribute('rotation', rotationStr);            
+
+            
+        } else {
+          var el = this.el;
+          var center = el.getAttribute('position');
+
+          var angleRad = this.getRandomAngleInRadians();
+          var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
+          var worldPoint = {x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z};
+          el.setAttribute('position', worldPoint);
+
+          var angleDeg = angleRad * 180 / Math.PI;
+          var angleToCenter = -1 * angleDeg + 90;
+          var rotationStr = '0 ' + angleToCenter + ' 0';
+          el.setAttribute('rotation', rotationStr);
+        }
+
+    },
 
   getRandomAngleInRadians: function() {
     return Math.random()*Math.PI*2;
@@ -27,4 +49,20 @@ AFRAME.registerComponent('spawn-in-circle', {
     y = Math.sin(angleRad)*radius;
     return {x: x, y: y};
   }
+
+
+/*update: function(){
+
+    },
+
+    tick: function(){
+
+
+
+    },
+
+    remove: function(){
+
+    }*/
+
 });
